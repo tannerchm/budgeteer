3cc095ebd352bbafab21d5a12df9b2bb3887bdd4
Tanner <tannerchm@yahoo.com>
2018-07-14 11:43:52 -0500

    almost final

---
+++ config/connection.js
@@ -0,0 +1,36 @@
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "",
+  //if error delete password.
+  database: "budgeteer"
+});
+
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    //if error delete password.
+    database: "budgeteer"
+  })
+}
+
+
+
+module.exports = connection;
