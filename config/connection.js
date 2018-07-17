949dea95d7094325963a63f94aea0922b918ee3b
Tanner <tannerchm@yahoo.com>
2018-07-16 19:06:22 -0500

    added jawsdb reqs

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,20 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  //if error delete password.
-  database: "budgeteer"
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    //if error delete password.
+    database: "budgeteer"
+  });
+  };
+
 
 
 connection.connect(function(err) {
